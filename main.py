d5e38196ed863c11cea47454d1e0552508108106
niocynth <dev@niocynth.io>
2025-08-19 16:14:13 +0100

    C2L1 - Added --verbose and copy/pasted calculator

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+if len(sys.argv) == 1:
+    print("Error: Prompt not found")
+    sys.exit(1)
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 def main():
-    if len(sys.argv) == 1:
-        print("Error: Prompt not found")
-        sys.exit(1)
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    model='gemini-2.0-flash-001', contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
     main()
-    
