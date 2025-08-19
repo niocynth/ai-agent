ba01634e09c517e0c7efdb8ad263c9ccdc7f0a83
niocynth <dev@niocynth.io>
2025-08-19 12:49:29 +0100

    C1L4 - Prompt via CLI argument, added error handling for missing args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,8 +8,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if len(sys.argv) == 1:
+        print("Error: Prompt not found")
+        sys.exit(1)
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=sys.argv[1]
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
@@ -17,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
